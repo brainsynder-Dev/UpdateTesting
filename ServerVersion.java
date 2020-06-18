d5d064a7ae26db7eaa59c85ecea75f1f307ee22a
brainsynder <briansnyder498@gmail.com>
2020-06-18 02:30:05 -0400

    Update ServerVersion.java

--- ServerVersion.java
+++ ServerVersion.java
@@ -5,26 +5,15 @@ import org.bukkit.Bukkit;
 public enum ServerVersion {
     // ---- START ---- //
     UNKNOWN,
-    v,
     v1_8_R3,
-    v,
     v1_9_R1,
-    v,
     v1_9_R2,
-    v,
     v1_10_R1,
-    v,
     v1_11_R1,
-    v,
     v1_12_R1,
-    v,
     v1_13_R1,
-    v,
     v1_13_R2,
-    v,
-    v1_14_R1,
-    v,
-    v
+    v1_14_R1
     ; // ---- END ---- //
 
     /**
